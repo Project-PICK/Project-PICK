8de46622a33c2aff477b76c844dafa76e3ecc355
Candace <cdm103@humboldt.edu>
2020-09-24 12:23:12 -0700

    Started initial GUI setup in WinForms (See issue #3 ) -added SplashPage.Designer.cs prototype -added LoginPage.Designer.cs prototype
    Signed-off-by: Candace Moore <cdm103@humboldt.edu>

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Project_PICK
+namespace PICKTrainingInc
 {
     static class Program
     {
@@ -16,7 +16,8 @@ namespace Project_PICK
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SplashPage());
+            //Application.Run(new LoginPage());
         }
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Project_PICK.Properties
+namespace PICKTrainingInc.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Project_PICK.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project_PICK.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PICKTrainingInc.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
