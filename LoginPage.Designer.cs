0e52fbc77cedb751f7d77d2d588186899f9be859
smittythehippy <kws36@humboldt.edu>
2020-10-25 19:09:22 -0700

    feat(forms):adding hidden password

--- LoginPage.Designer.cs
+++ LoginPage.Designer.cs
@@ -33,6 +33,7 @@ namespace PICKTrainingInc
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
             this.UsernameLabel = new System.Windows.Forms.Label();
             this.SignIn = new System.Windows.Forms.GroupBox();
+            this.registerButton = new System.Windows.Forms.Button();
             this.submitButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tb_password = new System.Windows.Forms.TextBox();
@@ -46,7 +47,7 @@ namespace PICKTrainingInc
             this.userOptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.Logo = new System.Windows.Forms.PictureBox();
-            this.registerButton = new System.Windows.Forms.Button();
+            this.showPassword = new System.Windows.Forms.CheckBox();
             this.SignIn.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
@@ -58,35 +59,44 @@ namespace PICKTrainingInc
             // 
             this.UsernameLabel.AutoSize = true;
             this.UsernameLabel.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UsernameLabel.Location = new System.Drawing.Point(4, 0);
-            this.UsernameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.UsernameLabel.Location = new System.Drawing.Point(3, 0);
             this.UsernameLabel.Name = "UsernameLabel";
-            this.UsernameLabel.Size = new System.Drawing.Size(176, 39);
+            this.UsernameLabel.Size = new System.Drawing.Size(116, 25);
             this.UsernameLabel.TabIndex = 2;
             this.UsernameLabel.Text = "Username";
             // 
             // SignIn
             // 
+            this.SignIn.Controls.Add(this.showPassword);
             this.SignIn.Controls.Add(this.registerButton);
             this.SignIn.Controls.Add(this.submitButton);
             this.SignIn.Controls.Add(this.tableLayoutPanel1);
             this.SignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SignIn.Location = new System.Drawing.Point(350, 122);
-            this.SignIn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SignIn.Location = new System.Drawing.Point(233, 79);
             this.SignIn.Name = "SignIn";
-            this.SignIn.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.SignIn.Size = new System.Drawing.Size(700, 320);
+            this.SignIn.Size = new System.Drawing.Size(467, 208);
             this.SignIn.TabIndex = 3;
             this.SignIn.TabStop = false;
             this.SignIn.Text = "Sign-In";
             // 
+            // registerButton
+            // 
+            this.registerButton.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold);
+            this.registerButton.Location = new System.Drawing.Point(6, 170);
+            this.registerButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(117, 33);
+            this.registerButton.TabIndex = 5;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
             // submitButton
             // 
             this.submitButton.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.submitButton.Location = new System.Drawing.Point(516, 260);
-            this.submitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.submitButton.Location = new System.Drawing.Point(344, 169);
             this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(176, 51);
+            this.submitButton.Size = new System.Drawing.Size(117, 33);
             this.submitButton.TabIndex = 4;
             this.submitButton.Text = "Submit";
             this.submitButton.UseVisualStyleBackColor = true;
@@ -101,40 +111,41 @@ namespace PICKTrainingInc
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.UsernameLabel, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.tb_userName, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 62);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 40);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(682, 154);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(455, 100);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
             // tb_password
             // 
-            this.tb_password.Location = new System.Drawing.Point(347, 85);
-            this.tb_password.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.tb_password.Location = new System.Drawing.Point(231, 55);
+            this.tb_password.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tb_password.Name = "tb_password";
-            this.tb_password.Size = new System.Drawing.Size(328, 30);
+            this.tb_password.Size = new System.Drawing.Size(220, 22);
             this.tb_password.TabIndex = 5;
+            this.tb_password.UseSystemPasswordChar = true;
+            this.tb_password.TextChanged += new System.EventHandler(this.tb_password_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(4, 77);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(3, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 39);
+            this.label1.Size = new System.Drawing.Size(111, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Password";
             // 
             // tb_userName
             // 
-            this.tb_userName.Location = new System.Drawing.Point(347, 8);
-            this.tb_userName.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.tb_userName.Location = new System.Drawing.Point(231, 5);
+            this.tb_userName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tb_userName.Name = "tb_userName";
-            this.tb_userName.Size = new System.Drawing.Size(328, 30);
+            this.tb_userName.Size = new System.Drawing.Size(220, 22);
             this.tb_userName.TabIndex = 4;
             // 
             // statusStrip1
@@ -142,36 +153,35 @@ namespace PICKTrainingInc
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusStrip});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 593);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 384);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(1068, 32);
+            this.statusStrip1.Size = new System.Drawing.Size(712, 22);
             this.statusStrip1.TabIndex = 4;
             this.statusStrip1.Text = "Login Please";
             // 
             // statusStrip
             // 
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(110, 25);
+            this.statusStrip.Size = new System.Drawing.Size(73, 17);
             this.statusStrip.Text = "Login Please";
             // 
             // menuStrip1
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exitToolStripMenuItem,
             this.userOptionsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1068, 35);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(712, 24);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(55, 29);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // userOptionsToolStripMenuItem
@@ -179,13 +189,13 @@ namespace PICKTrainingInc
             this.userOptionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.adminToolStripMenuItem});
             this.userOptionsToolStripMenuItem.Name = "userOptionsToolStripMenuItem";
-            this.userOptionsToolStripMenuItem.Size = new System.Drawing.Size(132, 29);
+            this.userOptionsToolStripMenuItem.Size = new System.Drawing.Size(87, 22);
             this.userOptionsToolStripMenuItem.Text = "User Options";
             // 
             // adminToolStripMenuItem
             // 
             this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
-            this.adminToolStripMenuItem.Size = new System.Drawing.Size(167, 34);
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
             this.adminToolStripMenuItem.Text = "Admin";
             // 
             // Logo
@@ -194,41 +204,40 @@ namespace PICKTrainingInc
             this.Logo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Logo.Image = ((System.Drawing.Image)(resources.GetObject("Logo.Image")));
             this.Logo.InitialImage = ((System.Drawing.Image)(resources.GetObject("Logo.InitialImage")));
-            this.Logo.Location = new System.Drawing.Point(18, 122);
-            this.Logo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Logo.Location = new System.Drawing.Point(12, 79);
             this.Logo.Name = "Logo";
-            this.Logo.Size = new System.Drawing.Size(322, 319);
+            this.Logo.Size = new System.Drawing.Size(215, 208);
             this.Logo.TabIndex = 1;
             this.Logo.TabStop = false;
             // 
-            // registerButton
+            // showPassword
             // 
-            this.registerButton.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold);
-            this.registerButton.Location = new System.Drawing.Point(9, 261);
-            this.registerButton.Name = "registerButton";
-            this.registerButton.Size = new System.Drawing.Size(176, 51);
-            this.registerButton.TabIndex = 5;
-            this.registerButton.Text = "Register";
-            this.registerButton.UseVisualStyleBackColor = true;
-            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            this.showPassword.AutoSize = true;
+            this.showPassword.Location = new System.Drawing.Point(267, 148);
+            this.showPassword.Name = "showPassword";
+            this.showPassword.Size = new System.Drawing.Size(136, 20);
+            this.showPassword.TabIndex = 6;
+            this.showPassword.Text = "Show Password";
+            this.showPassword.UseVisualStyleBackColor = true;
+            this.showPassword.CheckedChanged += new System.EventHandler(this.showPassword_CheckedChanged);
             // 
             // LoginPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1068, 625);
+            this.ClientSize = new System.Drawing.Size(712, 406);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.SignIn);
             this.Controls.Add(this.Logo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "LoginPage";
             this.Text = "User Login Page";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.closeForm);
             this.Load += new System.EventHandler(this.LoginPage_Load);
             this.SignIn.ResumeLayout(false);
+            this.SignIn.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
@@ -261,5 +270,6 @@ namespace PICKTrainingInc
         private System.Windows.Forms.ToolStripMenuItem userOptionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
         private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.CheckBox showPassword;
     }
 }
