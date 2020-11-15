b3d235d5358d1404c4629e440392b2788526c2df
Isaac Assegai Travers <isaac.a.travers@gmail.com>
2020-11-15 11:09:23 -0800

    Add blank dashboard
    This is what we will be building our user/admin
    dashboard from.
    
    This starts addressing issue #94
    
    Signed-off-by: Isaac Assegai Travers <isaac.a.travers@gmail.com>

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PICKTrainingInc
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuOptions = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.navigationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chooseTrainingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuOptions
+            // 
+            this.menuOptions.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuOptions.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuOptions.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.navigationToolStripMenuItem});
+            this.menuOptions.Location = new System.Drawing.Point(0, 0);
+            this.menuOptions.Name = "menuOptions";
+            this.menuOptions.Size = new System.Drawing.Size(1149, 33);
+            this.menuOptions.TabIndex = 19;
+            this.menuOptions.Text = "menuStrip2";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(60, 29);
+            this.exitToolStripMenuItem.Text = "Exit ";
+            // 
+            // navigationToolStripMenuItem
+            // 
+            this.navigationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dashboardToolStripMenuItem,
+            this.chooseTrainingToolStripMenuItem,
+            this.logoutToolStripMenuItem});
+            this.navigationToolStripMenuItem.Name = "navigationToolStripMenuItem";
+            this.navigationToolStripMenuItem.Size = new System.Drawing.Size(114, 29);
+            this.navigationToolStripMenuItem.Text = "Navigation";
+            // 
+            // dashboardToolStripMenuItem
+            // 
+            this.dashboardToolStripMenuItem.Name = "dashboardToolStripMenuItem";
+            this.dashboardToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.dashboardToolStripMenuItem.Text = "Dashboard";
+            // 
+            // chooseTrainingToolStripMenuItem
+            // 
+            this.chooseTrainingToolStripMenuItem.Name = "chooseTrainingToolStripMenuItem";
+            this.chooseTrainingToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.chooseTrainingToolStripMenuItem.Text = "Choose Training";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 638);
+            this.Controls.Add(this.menuOptions);
+            this.Name = "Dashboard";
+            this.Text = "Form1";
+            this.menuOptions.ResumeLayout(false);
+            this.menuOptions.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuOptions;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem navigationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dashboardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chooseTrainingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+    }
+}
---
+++ Dashboard.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * The Dashboard will show the user all their stats
+ * and will show the administrator ALL user stats
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PICKTrainingInc
+{
+    public partial class Dashboard : Form
+    {
+        /* Field Variables and Objects */
+        DataBaseManager databaseManager;
+        StateManager stateManager;
+
+        /**
+         * Constructor
+         * */
+        public Dashboard(DataBaseManager dbManager, StateManager sManager)
+        {
+            InitializeComponent();
+            databaseManager = dbManager;
+            stateManager = sManager;
+        }
+    }
+}
